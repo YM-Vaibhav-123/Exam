fcd5342cdeab6f4de9e982717895f4ba7d48d869
Vaibhav Wandhare <vaibhav.wandhare@yuktamedia.com>
2021-01-05 20:33:55 +0530

    Test

---
+++ PrintStructureTest.java
@@ -0,0 +1,22 @@
+package AssessmentTest;
+
+public class PrintStructureTest {
+
+	public static void main(String[] args) {
+		
+		int i,j,k=4;
+		
+		for(i=0;i<k;i++) {
+			
+			for(j=1;j<=i;j++) {
+				
+				System.out.println("*");
+			}
+			
+			System.out.println();
+		}
+		// TODO Auto-generated method stub
+
+	}
+
+}
